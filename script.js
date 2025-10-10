b3667819e5c1566fdf79a0ff1df9c457a47b0e11
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2025-10-10 15:40:00 +1100

    update strings

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myString = "This is a string"
-let anotherString = "   Another string"
-let hello = "Hello there!"
-let myName = "Keegan"
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Keegan";
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
